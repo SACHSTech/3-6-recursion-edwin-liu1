09077a570db37d7527f759d252c3137885bd4d30
Edwin_liu1 <edwin.liu25@ycdsbk12,ca>
2025-05-14 20:27:15 -0400

    isPalindrome done

--- src/recursion/CheckStringPalindrome.java
+++ src/recursion/CheckStringPalindrome.java
@@ -2,4 +2,19 @@ package recursion;
 
 public class CheckStringPalindrome {
     
+    public static boolean isPalindrome(String s){
+        boolean palindrome = true;
+        if (!s.isEmpty()){
+            if (s.charAt(0) == s.charAt(s.length()-1)){
+                isPalindrome(s.substring(1, s.length()-2));
+            } else {
+                palindrome = false;
+            }
+        }
+        return palindrome;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("racecar")); // output: true
+        System.out.println(isPalindrome("train")); // output: false
+    }
 }
