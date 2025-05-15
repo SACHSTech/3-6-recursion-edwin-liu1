a5aabe52cfedd9c823ab914a84e5ce95c1aec992
Edwin_liu1 <edwin.liu25@ycdsbk12,ca>
2025-05-14 20:34:31 -0400

    PowerFunction done

--- src/recursion/PowerFunction.java
+++ src/recursion/PowerFunction.java
@@ -1,5 +1,17 @@
 package recursion;
 
 public class PowerFunction {
-    
+
+    public static int power(int base, int exponent){
+        int num = base;
+        if (exponent > 1){
+            num = power(base, exponent-1);
+            num *= base;
+        }
+        return num;
+    }
+    public static void main(String[] args) {
+        System.out.println(power(2, 3)); // output: 8
+        System.out.println(power(3, 6)); // output: 729
+    }
 }
