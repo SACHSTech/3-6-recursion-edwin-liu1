6c7213bf9a54191ef047e67e560333da76039e7d
Edwin_liu1 <edwin.liu25@ycdsbk12,ca>
2025-05-14 20:18:44 -0400

    ReverseString done

--- src/recursion/ReverseString.java
+++ src/recursion/ReverseString.java
@@ -2,4 +2,16 @@ package recursion;
 
 public class ReverseString {
     
+    public static String reverse(String s){
+        String reverse = "";
+        reverse += s.charAt(s.length()-1);
+        if (!(s.length()<=1)){
+            reverse += reverse(s.substring(0, s.length()-1));
+        }
+        return reverse;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("dog")); // output: god
+    }
 }
